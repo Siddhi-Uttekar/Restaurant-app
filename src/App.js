137507a6d223e0a96108284be7ffe36952220acd
Siddhi-Uttekar <uttekarsiddhi145@gmail.com>
2024-07-14 20:10:15 +0530

    Restaurant app using react

--- src/App.js
+++ src/App.js
@@ -14,10 +14,52 @@
  * -contact
  */
 
-const AppLayout = () => {
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './style.css';
+
+const Header = () => {
     return (
-        <div>
-            
+        <div className="header">
+           <div className="logo-container">
+             <img className="logo" src="https://static.vecteezy.com/system/resources/previews/009/291/628/non_2x/restaurant-logo-design-vector.jpg">
+             </img>
+           </div>
+           <div className="nav-items">
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+            </ul>
+
+           </div>
         </div>
     )
 }
+
+const Body = () => {
+    return(
+        <div className='body'>
+            <div className='Search'></div>
+            <div className='res-container'>
+                
+            </div>
+
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header/>
+            <Body/>
+
+
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
